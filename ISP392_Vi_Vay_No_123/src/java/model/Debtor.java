6441f753ba365a7c094f689ef413314367d8e9a1
MinhHau1743 <Minhhaunguyen065@gmail.com>
2024-02-16 18:27:47 +0700

    debtor
    edit model debtor

--- ISP392_Vi_Vay_No_123/src/java/model/Debtor.java
+++ ISP392_Vi_Vay_No_123/src/java/model/Debtor.java
@@ -4,29 +4,37 @@
  */
 package model;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
 import java.security.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author MINIMONIE
  */
 public class Debtor {
-    private int idDebtor;
+    private int id;
     private String name;
     private String address;
     private String phone;
     private String email;
     private double totalDebt;
-    private Timestamp createdAt;
-    private Timestamp updatedAt;
+    private java.sql.Timestamp createdAt;
+    private java.sql.Timestamp updatedAt;
     private int account_id;
     private int history_payment_id;
 
     public Debtor() {
     }
 
-    public Debtor(int idDebtor, String name, String address, String phone, String email, double totalDebt, Timestamp createdAt, Timestamp updatedAt, int account_id, int history_payment_id) {
-        this.idDebtor = idDebtor;
+    public Debtor(int id, String name, String address, String phone, String email, double totalDebt, java.sql.Timestamp createdAt, java.sql.Timestamp updatedAt, int account_id, int history_payment_id) {
+        this.id = id;
         this.name = name;
         this.address = address;
         this.phone = phone;
@@ -38,12 +46,28 @@ public class Debtor {
         this.history_payment_id = history_payment_id;
     }
 
-    public int getIdDebtor() {
-        return idDebtor;
+        /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+    public Debtor(String name, String address, String phone, String email, double totalDebt, int account_id) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.totalDebt = totalDebt;
+        this.account_id = account_id;
     }
 
-    public void setIdDebtor(int idDebtor) {
-        this.idDebtor = idDebtor;
+   
+
+    public int getIdDebtor() {
+        return id;
+    }
+
+    public void setIdDebtor(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -86,19 +110,19 @@ public class Debtor {
         this.totalDebt = totalDebt;
     }
 
-    public Timestamp getCreatedAt() {
+    public java.sql.Timestamp getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(Timestamp createdAt) {
+    public void setCreatedAt(java.sql.Timestamp createdAt) {
         this.createdAt = createdAt;
     }
 
-    public Timestamp getUpdatedAt() {
+    public java.sql.Timestamp getUpdatedAt() {
         return updatedAt;
     }
 
-    public void setUpdatedAt(Timestamp updatedAt) {
+    public void setUpdatedAt(java.sql.Timestamp updatedAt) {
         this.updatedAt = updatedAt;
     }
 
@@ -120,7 +144,7 @@ public class Debtor {
 
     @Override
     public String toString() {
-        return "Debtor{" + "idDebtor=" + idDebtor + ", name=" + name + ", address=" + address + ", phone=" + phone + ", email=" + email + ", totalDebt=" + totalDebt + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + ", account_id=" + account_id + ", history_payment_id=" + history_payment_id + '}';
+        return "Debtor{" + "id=" + id + ", name=" + name + ", address=" + address + ", phone=" + phone + ", email=" + email + ", totalDebt=" + totalDebt + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + ", account_id=" + account_id + ", history_payment_id=" + history_payment_id + '}';
     }
     
 }
