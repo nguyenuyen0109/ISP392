c759bb507ccddeddb72f3381b71a03d1974bd018
Minhnguyenhoa <101202463+Minhnguyenhoa@users.noreply.github.com>
2024-03-06 22:32:44 +0700

    fix feedbackForm
    fix feedbackForm

---
+++ ISP392_Vi_Vay_No_123/src/java/controller/FeedbackController.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dao.FeedbackDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Feedback;
+
+/**
+ *
+ * @author admin
+ */
+public class FeedbackController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet FeedbackController</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet FeedbackController at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("USER") == null) {
+            response.sendRedirect("login.jsp");
+            return;
+        }
+        Integer account_id = (Integer) session.getAttribute("account_id");
+        if (account_id == null) {
+
+            response.sendRedirect("login.jsp");
+            return;
+        }
+        try {
+            double rating = Double.parseDouble(request.getParameter("rating"));
+            String opinion = request.getParameter("opinion");
+
+            Feedback feedback = new Feedback();
+            FeedbackDAO dao = new FeedbackDAO();
+            feedback.setAccount_id(account_id);
+            feedback.setRate(rating);
+            feedback.setFeedback(opinion);
+            java.sql.Timestamp Timestamp = new java.sql.Timestamp(System.currentTimeMillis());
+            feedback.setCreateAt(Timestamp);
+            feedback.setIsDeleted(false);
+            dao.addFeedback(feedback);
+
+            response.sendRedirect("client/homepage.jsp");
+        } catch (Exception e) {
+
+            throw new ServletException(e);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
